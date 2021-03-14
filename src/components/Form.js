48136984f9d053c7758bc0a649e9b316e45c7cfb
Dan Chiev <danchiev2@Gmail.com>
2021-03-14 09:15:58 -0400

    added event handlers

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,27 @@
 import React from "react";
-const Form = () => {
+const Form = ({ setInputText, todos, setTodos, inputText }) => {
+  const inputTextHandler = (e) => {
+    console.log(e.target.value);
+    setInputText(e.target.value);
+  };
+
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
   return (
     <form>
-      <input type="text" className="todo-input" />
-      <button class="todo-button" type="submit">
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      />
+      <button onClick={submitTodoHandler} class="todo-button" type="submit">
         <i class="fas fa-plus-square"></i>
       </button>
       <div class="select">
